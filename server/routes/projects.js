823768196efa272739a00ebe6cbba049f25f3c30
Eric Skogen <audionerd@users.noreply.github.com>
2020-02-17 10:22:06 -0600

    extract importer service

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,93 +1,32 @@
-const path = require('path')
-const os = require('os')
-const fs = require('fs-extra')
-const AdmZip = require('adm-zip')
 const multiparty = require('multiparty')
 
-const { run, get, all } = require('../db')
-const { insertProject, importScene, importScript } = require('../importers')
-
-const UPLOADS_PATH = path.join(__dirname, '../../public/uploads')
-
-const readJson = (...rest) => JSON.parse(fs.readFileSync(...rest))
+const { get, all } = require('../db')
+const importer = require('../services/importer')
 
 exports.new = (req, res) => {
   res.render('project/new')
 }
 
 exports.create = (req, res, next) => {
-  let form = new multiparty.Form()
-  form.parse(req, function (err, fields, files) {
-    if (err) {
-      next(err)
-      return
-    }
+  new multiparty.Form().parse(req, function (err, fields, files) {
+    if (err) return next(err)
 
     let [name] = fields.name // project name
     let [file] = files.file // first attached file
 
-    if (!name) {
-      next(new Error('Missing name'))
-      return
-    }
+    if (!name) return next(new Error('Missing name'))
 
     if (!file || file.size == 0) {
-      next(new Error('Missing or invalid ZIP file'))
-      return
+      return next(new Error('Missing or invalid ZIP file'))
     }
 
     try {
-      let zip = new AdmZip(file.path)
-
-      let tmpPathPrefix = path.join(os.tmpdir(), 'storyboarder-')
-
-      let folder = fs.mkdtempSync(tmpPathPrefix)
-
-      zip.extractAllTo(folder)
-
-      let scriptFilename = fs.readdirSync(folder).find(
-        filename => filename.toLowerCase().endsWith('.fountain')
-      )
-      if (scriptFilename) {
-        let scriptPath = path.join(folder, scriptFilename)
-        let name = path.basename(scriptPath, '.fountain')
-        let projectId = run(...insertProject({ name, scriptPath })).lastInsertRowid
-        let pathToFountainFile = `projects/${projectId}/${scriptFilename}`
-
-        importScript(run, {
-          projectId,
-          script: fs.readFileSync(scriptPath, 'utf-8'),
-          scriptPath,
-          pathToFountainFile
-        })
-
-        let uploadsPath = path.join(UPLOADS_PATH, path.dirname(pathToFountainFile))
-        fs.mkdirpSync(uploadsPath)
-        fs.copySync(
-          folder,
-          uploadsPath,
-          { preserveTimestamps: true, overwrite: false }
-        )
-
-        res.redirect(`/projects/${projectId}`)
-        return
-      } else {
-        let storyboarderFile = fs.readdirSync(folder).find(filename => filename.toLowerCase().endsWith('.storyboarder'))
-
-        if (storyboarderFile) {
-          next(new Error('Not implemented yet. Try a script-based project.'))
-          return
-        } else {
-          next(new Error('Invalid ZIP. No .fountain or .storyboarder files found.'))
-          return
-        }
-      }
+      let { redirectUri } = importer({ pathToZip: file.path })
+      return res.redirect(redirectUri)
     } catch (err) {
-      next(err)
-      return
+      return next(err)
     }
   })
-  return
 }
 
 exports.show = (req, res) => {
---
+++ server/services/importer.js
@@ -0,0 +1,65 @@
+const AdmZip = require('adm-zip')
+const fs = require('fs-extra')
+const os = require('os')
+const path = require('path')
+
+const { run } = require('../db')
+const { insertProject, importScene, importScript } = require('../importers')
+
+const UPLOADS_PATH = path.join(__dirname, '../../public/uploads')
+
+const readJson = (...rest) => JSON.parse(fs.readFileSync(...rest))
+
+module.exports = function ({ pathToZip }) {
+  let tmpPathPrefix = path.join(os.tmpdir(), 'storyboarder-')
+  let folder = fs.mkdtempSync(tmpPathPrefix)
+
+  let zip = new AdmZip(pathToZip)
+
+  zip.extractAllTo(folder)
+
+  let scriptFilename = fs.readdirSync(folder).find(
+    filename => filename.toLowerCase().endsWith('.fountain')
+  )
+  if (scriptFilename) {
+    let scriptPath = path.join(folder, scriptFilename)
+    let name = path.basename(scriptPath, '.fountain')
+    let projectId = run(...insertProject({ name, scriptPath })).lastInsertRowid
+    let pathToFountainFile = `projects/${projectId}/${scriptFilename}`
+
+    importScript(run, {
+      projectId,
+      script: fs.readFileSync(scriptPath, 'utf-8'),
+      scriptPath,
+      pathToFountainFile
+    })
+
+    let uploadsPath = path.join(UPLOADS_PATH, path.dirname(pathToFountainFile))
+    fs.mkdirpSync(uploadsPath)
+    fs.copySync(
+      folder,
+      uploadsPath,
+      { preserveTimestamps: true, overwrite: false }
+    )
+
+    return {
+      redirectUri: `/projects/${projectId}`
+    }
+  } else {
+    let storyboarderFile = fs.readdirSync(folder).find(filename => filename.toLowerCase().endsWith('.storyboarder'))
+
+    if (storyboarderFile) {
+      // let storyboarderPath = path.join(folder, storyboarderFile)
+      // let projectId = run(...insertProject({ name })).lastInsertRowid
+      // importScene(run, {
+      //   scene: readJson(storyboarderPath),
+      //   storyboarderPath: '', // e.g.: 'scenes/${sceneId}/example.storyboarder',
+      //   projectId
+      // })
+      return new Error('Not implemented yet. Try a script-based project.')
+    } else {
+      return new Error('Invalid ZIP. No .fountain or .storyboarder files found.')
+      return
+    }
+  }
+}
