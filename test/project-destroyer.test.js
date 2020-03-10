ca9f1aeac344815117244a7e59e6d5406cc4d5a0
Eric Skogen <audionerd@users.noreply.github.com>
2020-03-10 10:32:36 -0500

    fix test

--- test/project-destroyer.test.js
+++ test/project-destroyer.test.js
@@ -15,25 +15,29 @@ mock({
   },
   [FIXTURES_PATH]: {
     'zip': {
-      'multi-scene.zip': fs.readFileSync(path.join(FIXTURES_PATH, 'zip/multi-scene.zip'))
+      'multi-scene.zip': fs.readFileSync(path.join(__dirname, '../tmp/multi-scene.zip'))
     }
   }
 })
+let { uri: redirectUri } = importer({
+  pathToZip: path.resolve(path.join(FIXTURES_PATH, 'zip/multi-scene.zip'))
+})
+let projectId = Number(redirectUri.match(/\d+$/)[0])
 
 t.test('can destroy a script-based project', t => {
-  let { uri: redirectUri } = importer({
-    pathToZip: path.resolve(path.join(FIXTURES_PATH, 'zip/multi-scene.zip'))
-  })
-  let projectId = Number(redirectUri.match(/\d+$/)[0])
-
+  // source exists
   t.equal(true, fs.existsSync(path.join(UPLOADS_PATH, redirectUri)))
   t.equal(2, fs.readdirSync(path.join(UPLOADS_PATH, redirectUri)).length)
 
+  // run the destroyer
   projectDestroyer({ projectId })
 
+  // source does not exist
   t.equal(false, fs.existsSync(path.join(UPLOADS_PATH, redirectUri)))
 
   t.end()
 })
 
-mock.restore()
+t.teardown(() => {
+  mock.restore()
+})
