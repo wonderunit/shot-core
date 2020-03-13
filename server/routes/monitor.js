0c93035e876acd58382b850bf1b68b5de1c6e71e
Eric Skogen <audionerd@users.noreply.github.com>
2020-03-12 19:04:12 -0500

    monitor: more real data

--- server/routes/monitor.js
+++ server/routes/monitor.js
@@ -1,3 +1,42 @@
-exports.show = (req, res) => {
-  res.render('monitor')
+const { get, all, run } = require('../db')
+
+const Shot = require('../decorators/shot')
+const Scene = require('../decorators/scene')
+
+exports.show = (req, res, next) => {
+  let { projectId } = req.params
+
+  let project = get('SELECT * FROM projects WHERE id = ?', projectId)
+
+  let event = get('SELECT * FROM events WHERE id = ?', project.slater_event_id)
+  let shot = get('SELECT * FROM shots WHERE id = ?', event.shot_id)
+  let scene = get('SELECT * FROM scenes WHERE id = ?', shot.scene_id)
+  let takes = all('SELECT * FROM takes WHERE shot_id = ?', shot.id)
+
+  scene = new Scene(scene)
+  shot = new Shot(shot)
+
+  // TODO
+  let prevShot = shot
+  let prevScene = scene
+  let nextShot = shot
+  let nextScene = scene
+
+  let takeNumber = takes.length + 1
+
+  let aspectRatio = scene.metadata.aspectRatio
+
+  res.render('monitor', {
+    project,
+    event,
+    shot,
+    scene,
+    
+    takeNumber,
+    prevShot,
+    nextShot,
+    prevScene,
+    nextScene,
+
+    aspectRatio })
 }
