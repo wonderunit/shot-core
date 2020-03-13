2de4092d09351f5d659af34e08c8bec795254052
Eric Skogen <audionerd@users.noreply.github.com>
2020-03-13 15:05:09 -0500

    populate day start_ats

--- server/decorators/day.js
+++ server/decorators/day.js
@@ -4,34 +4,47 @@ const { format } = require('date-fns-tz')
 const asDateOrNull = require('../../lib/asDateOrNull')
 
 class Day {
-  static decorateCollection (collection, { events }) {
-    let lastStartAt = asDateOrNull(collection[0].start_at)
+  /*
+  given a complete array of all Day objects, ordered by rank
+    where the first Day MUST have a valid start_at
+      and subquent Days MAY have a valid start_at
+  populate the start_art value for each
+  */
+  static assignStartAts (collection) {
+    if (collection[0].start_at == null) throw new Error('First day has invalid start_at')
 
-    return collection.map((curr, n, arr) => {
+    let curr
+
+    return collection.map(day => {
+      if (day.start_at) {
+        curr = new Date(day.start_at)
+      } else {
+        curr = addDays(curr, 1)
+      }
+
+      return {
+        ...day,
+        start_at: curr
+      }
+    })
+  }
+
+  static decorateCollection (collection, { events }) {
+    return Day.assignStartAts(collection.map((curr, n, arr) => {
       let next = arr[n + 1]
 
       let dayEvents = events.filter(
         event => event.rank > curr.rank && (next ? event.rank < next.rank : true)
       )
 
-      curr.start_at = asDateOrNull(curr.start_at)
-      if (curr.start_at) {
-        lastStartAt = curr.start_at
-      } else {
-        lastStartAt = addDays(lastStartAt, 1)
-      }
-
-      let displayDate = asDateOrNull(curr.start_at || lastStartAt)
-
       return new Day({
         ...curr,
         day_number: n + 1,
         days_total: arr.length,
         event_ids: dayEvents.map(event => event.id),
-        shot_count: dayEvents.filter(event => event.shot_id != null).length,
-        displayDate: displayDate ? format(displayDate, 'EEEE, dd MMM yyyy') : null
+        shot_count: dayEvents.filter(event => event.shot_id != null).length
       })
-    })
+    }))
   }
 
   constructor (event) {
