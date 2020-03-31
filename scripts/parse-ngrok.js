d13714e29aa6cbaf89fdc6d9cb3adf8983374692
Eric Skogen <audionerd@users.noreply.github.com>
2020-03-31 09:48:47 -0500

    Update parse-ngrok.js

--- scripts/parse-ngrok.js
+++ scripts/parse-ngrok.js
@@ -7,18 +7,22 @@
     for await (const input of process.stdin) {
       let urlHttp = url.parse(input.match(/Forwarding\s+(.+) -> http:.+:80/)[1])
       let urlWs = url.parse(input.match(/Forwarding\s+(.+) -> http:.+:81/)[1])
+      let urlRtsp = url.parse(input.match(/Forwarding\s+(.+) -> .+:554/)[1])
 
-      let httpUri = urlHttp.href.slice(0, -1)
-      let wsUri = urlWs.href.slice(0, -1)
+      let httpUri = urlHttp.href.replace(/\/$/, '')
+      let wsUri = urlWs.href.replace(/\/$/, '')
+      let rtspUri = `rtsp://${urlRtsp.hostname}:${urlRtsp.port}`
 
       console.log(bold('Z Cam ngrok tunnels:'))
       console.log('http   ', httpUri)
       console.log('ws     ', wsUri)
+      console.log('rtsp   ', rtspUri)
       console.log('')
 
       console.log(bold('As env vars:'))
       console.log(`ZCAM_URL=${httpUri}`)
       console.log(`ZCAM_WS_URL=${wsUri}`)
+      console.log(`ZCAM_RTSP_URL=${rtspUri}`)
       console.log('')
 
       console.log(bold('Test HTTP via curl:'))
@@ -29,13 +33,27 @@
       console.log('wscat -c $ZCAM_WS_URL')
       console.log('')
 
+      console.log(bold('Test RTSP via curl:'))
+      console.log('curl -v $ZCAM_RTSP_URL')
+      console.log('')
+
+      console.log(bold('Play RTSP stream:'))
+      console.log('vlc $ZCAM_RTSP_URL/live_stream')
+      console.log('')
+
       console.log(bold('Run Shot Core server:'))
-      console.log(`ZCAM_URL=${urlHttp.href.slice(0, -1)} ZCAM_WS_URL=${urlWs.href.slice(0, -1)} npm start`)
+      console.log(`
+ZCAM_URL=${httpUri} \\
+ZCAM_WS_URL=${wsUri} \\
+ZCAM_RTSP_URL=${rtspUri} \\
+npm start
+      `.trim())
       console.log('')
 
       console.log(bold('Forward to localhost :80/:81:'))
       console.log(`ncat --sh-exec "ncat ${urlHttp.hostname} 80" -l localhost 80 --keep-open`)
       console.log(`ncat --sh-exec "ncat ${urlWs.hostname} 80" -l localhost 81 --keep-open`)
+      console.log(`ncat --sh-exec "ncat ${urlRtsp.hostname} ${urlRtsp.port}" -l localhost 554 --keep-open`)
     }
   } catch (err) {
     console.error('Could not parse STDIN')
