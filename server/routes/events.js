cae5e782b0dacb6d449dbba3f674063ea4790b17
Eric Skogen <audionerd@users.noreply.github.com>
2020-04-30 11:54:49 -0500

    extract createEvent service

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,41 +1,18 @@
 const { run, get } = require('../db')
+const createEvent = require('../services/create-event')
 
 exports.create = (req, res, next) => {
   let { projectId } = req.params
-  let { insertAfter, eventType, description = '' } = req.body
-
   projectId = parseInt(projectId)
 
-  let prev = get('SELECT * FROM events WHERE id = ?', insertAfter)
+  let { insertAfter, eventType, description = '' } = req.body
 
-  let rank = prev.rank + 1
-
-  // re-rank (push all items down to make a gap for the new item)
-  let rankings = [
-    `UPDATE events
-     SET rank = rank + 1
-     WHERE rank >= ?
-     AND project_id = ?`,
-     rank,
-     projectId
-  ]
-
-  // insert the new event
-  let insertEvent = [
-    `INSERT INTO events
-      (project_id, rank, event_type, description)
-    VALUES
-      (?, ?, ?, ?)`,
-    [
-      projectId,
-      rank,
-      eventType,
-      description
-    ]
-  ]
-
-  run(...rankings)
-  let { changes, lastInsertRowId } = run(...insertEvent)
+  createEvent({
+    projectId,
+    insertAfter,
+    eventType,
+    description
+  })
 
   return res.status(201).send()
 }
---
+++ server/services/create-event.js
@@ -0,0 +1,41 @@
+const { run, get } = require('../db')
+
+module.exports = function createEvent ({
+    projectId,
+    insertAfter,
+    eventType,
+    description
+}) {
+  let prev = get('SELECT * FROM events WHERE id = ?', insertAfter)
+
+  let rank = prev.rank + 1
+
+  // re-rank (push all items down to make a gap for the new item)
+  let rankings = [
+    `UPDATE events
+     SET rank = rank + 1
+     WHERE rank >= ?
+     AND project_id = ?`,
+     rank,
+     projectId
+  ]
+
+  // insert the new event
+  let insertEvent = [
+    `INSERT INTO events
+      (project_id, rank, event_type, description)
+    VALUES
+      (?, ?, ?, ?)`,
+    [
+      projectId,
+      rank,
+      eventType,
+      description
+    ]
+  ]
+
+  run(...rankings)
+  let { changes, lastInsertRowId } = run(...insertEvent)
+
+  return get('SELECT * FROM events WHERE id = ?', lastInsertRowId)
+}
