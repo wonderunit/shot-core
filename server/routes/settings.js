e8639ff2807bb66ba0f80e25420ebab81e604fc3
Eric Skogen <audionerd@users.noreply.github.com>
2020-05-11 15:48:31 -0500

    settings: can save changes

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -1,15 +1,16 @@
-const { get } = require('../db')
+const { get, run } = require('../db')
+
+let ALLOWED = [
+  'zcam_wired_ip',
+  'zcam_wireless_ip',
+  'uploads_path',
+  'active_project_id'
+]
 
 const select = () => get(
-  `SELECT
-    zcam_wired_ip,
-    zcam_wireless_ip,
-    uploads_path,
-    active_project_id
+  `SELECT ${ALLOWED.join(',')}
   FROM settings
   LIMIT 1`)
-
-const insert = select
 
 exports.index = (req, res) => {
   const settings = select()
@@ -21,11 +22,30 @@ exports.index = (req, res) => {
 }
 
 exports.update = (req, res) => {
-  const settings = insert()
+  const before = select()
+
+  let changed = {}
+  for (let key of ALLOWED) {
+    if (req.body[key] != null) {
+      let value = req.body[key] == '' ? null : req.body[key]
+      if (value != before[key]) {
+        changed[key] = [value, before[key]]
+      }
+    }
+  }
+
+  for (let [key, [value, prev]] of Object.entries(changed)) {
+    run(`UPDATE settings SET ${key} = ?`, value)
+  }
+
+  let changelog = Object.entries(changed).map(([key, [value, prev]]) => `${key} changed from ${prev} to ${value}`)
+  changelog = changelog.join('<br />\n')
+
+  const after = select()
 
   res.render('settings', {
-    flash: 'Saved.',
-    ...settings,
+    flash: `Saved.<br /><small>` + changelog + '</small>',
+    ...after,
     default_uploads_path: require('../config').UPLOADS_PATH
   })
 }
