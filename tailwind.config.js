3e59a25b7030fd4aea332ce49307beb87c9237b4
Eric Skogen <audionerd@users.noreply.github.com>
2020-05-26 16:33:04 -0500

    start re-design using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,24 @@ module.exports = {
     'server/views/*.ejs'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xxs': '9px',
+        '7xl': ['4.5rem', 1],
+        '8xl': ['9rem', 1]
+      },
+      colors: {
+        purple: {
+          '100': '#F9F7FF',
+          '400': '#AB94FF',
+          '600': '#7B60DC'
+        }
+      }
+    },
   },
   variants: {},
   plugins: [],
+  corePlugins: {
+    fontFamily: false
+  }
 }
