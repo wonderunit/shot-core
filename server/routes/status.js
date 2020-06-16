a63bfc23c657d959789789b3a0fdd13b1f0645a5
Eric Skogen <audionerd@users.noreply.github.com>
2020-06-16 14:41:27 -0500

    status: link to .MOV on camera

--- server/routes/status.js
+++ server/routes/status.js
@@ -1,10 +1,12 @@
 const { run, get, all } = require('../db')
 
+const { ZCAM_URL } = require('../config')
 const Take = require('../decorators/take')
 
 exports.index = (req, res) => {
   let takes = all(`SELECT * from takes ORDER BY project_id, downloaded`)
   res.render('status', {
-    takes: Take.decorateCollection(takes)
+    takes: Take.decorateCollection(takes),
+    ZCAM_URL
   })
 }
